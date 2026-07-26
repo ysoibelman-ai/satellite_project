534742bd8f5235550aa56d4bb2e7ba99c84eb96d
yaakov-ai <ysoibelman@gmail.com>
2026-07-26 10:27:05 +0300

    validation

--- mainL1.py
+++ mainL1.py
@@ -1,4 +1,5 @@
 from space_network_lib import *
+from time import sleep
 
 class Satellite (SpaceEntity):
     def __int__(self,name: str, distance_from_earth: int | float):
@@ -8,9 +9,22 @@ class Satellite (SpaceEntity):
         print (f"{self.name} Received: {packet}")
 
 
-israel_space_network = SpaceNetwork(level = 1)
+israel_space_network = SpaceNetwork(level = 2)
 Sat1 = Satellite("Sat1", 100)
 Sat2 = Satellite("Sat2", 200)
 
+valid = False
 packet_1 = Packet ("this is an important secret message",Sat1,Sat2,)
-israel_space_network.send(packet_1)
+ 
+while valid == False:
+
+    try:
+          israel_space_network.send(packet_1)
+          valid = True
+    
+    except TemporalInterferenceError, DataCorruptedError:
+        continue
+
+
+
+
