dfff5e08d0b9fde441b2355c794b530ea09eb6be
yaakov-ai <ysoibelman@gmail.com>
2026-07-26 12:14:00 +0300

    added Packet_to_relay

--- mainL1.py
+++ mainL1.py
@@ -11,6 +11,16 @@ class Satellite (SpaceEntity):
 class BrokenConnectionError (CommsError):
     pass
 
+class RelayPacket (Packet):
+
+    def __init__(self, packet_to_relay, sender, proxy):
+        super().__init__(packet_to_relay, sender, proxy)
+       
+
+    def __repr__(self):
+        return f"RelayPacket(Relaying [{self.data}] to {self.receiver} from {self.sender})"
+    
+
 def attempt_transmission(packet: Packet):
 
     while True:
